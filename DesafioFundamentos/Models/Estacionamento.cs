51a8b8bfda8bfd795dc7f93bdea4c5687987cf56
Lucas Felipe Costa <lucasfelipecost4@gmail.com>
2023-11-02 19:57:20 -0300

    Listar as placa cadastradas

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -18,10 +18,11 @@ namespace DesafioFundamentos.Models
         {
             Console.WriteLine("Digite a placa do veículo para estacionar:");
             string placa = Console.ReadLine();
+            placa = placa.Replace("-", "").Trim().ToUpper();
 
             if (!ValidarPlaca(placa))
             {
-                Console.WriteLine("A placa do veículo digitada é invalida");
+                Console.WriteLine($"A placa do veículo digitada ({placa}) é invalida");
                 return;
             }
 
@@ -37,21 +38,14 @@ namespace DesafioFundamentos.Models
 
             placa = placa.Replace("-", "").Trim();
 
-            /*
-             *  Verifica se o caractere da posição 4 é uma letra, se sim, aplica a validação para o formato de placa do Mercosul,
-             *  senão, aplica a validação do formato de placa padrão.
-             */
             if (char.IsLetter(placa, 4))
             {
-                /*
-                 *  Verifica se a placa está no formato: três letras, um número, uma letra e dois números.
-                 */
-                var padraoMercosul = new Regex("[a-zA-Z]{3}[0-9]{1}[a-zA-Z]{1}[0-9]{2}");
+
+                Regex padraoMercosul = new Regex("[a-zA-Z]{3}[0-9]{1}[a-zA-Z]{1}[0-9]{2}");
                 return padraoMercosul.IsMatch(placa);
             }
 
-            // Verifica se os 3 primeiros caracteres são letras e se os 4 últimos são números.
-            var padraoNormal = new Regex("[a-zA-Z]{3}[0-9]{4}");
+            Regex padraoNormal = new Regex("[a-zA-Z]{3}[0-9]{4}");
             return padraoNormal.IsMatch(placa);
 
         }
@@ -89,16 +83,18 @@ namespace DesafioFundamentos.Models
         public void ListarVeiculos()
         {
             // Verifica se há veículos no estacionamento
-            if (veiculos.Any())
-            {
-                Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
-            }
-            else
+            if (!veiculos.Any())
             {
                 Console.WriteLine("Não há veículos estacionados.");
             }
+
+            Console.WriteLine("Os veículos estacionados são:");
+
+            foreach (var item in veiculos)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 }
